9d5c3322bf7de676f7fac189a69bb22d57aa4cf3
jeremychia <jeremy.chia@vinted.com>
2024-04-13 22:41:09 +0200

    feat: add fact model

---
+++ models/facts/fact_bank_transactions.sql
@@ -0,0 +1,26 @@
+with
+    union_all as (
+        {{ dbt_utils.union_relations(
+            relations=[
+                ref("stg_bank_de_eur_n26"),
+                ref("stg_bank_fr_eur_hsbcfr"),
+                ref("stg_bank_sg_eur_revolut_v1"),
+                ref("stg_bank_sg_eur_revolut_v2"),
+                ref("stg_bank_hk_hkd_hangseng"),
+                ref("stg_bank_sg_other_revolut_v2"),
+                ref("stg_bank_sg_sgd_adjustments_v1"),
+                ref("stg_bank_sg_sgd_citicc"),
+                ref("stg_bank_sg_sgd_dbs"),
+                ref("stg_bank_sg_sgd_hsbccc"),
+                ref("stg_bank_sg_sgd_ocbc_v1"),
+                ref("stg_bank_sg_sgd_revolut_v1"),
+                ref("stg_bank_sg_sgd_revolut_v2"),
+                ref("stg_bank_sg_sgd_scbcc"),
+                ref("stg_bank_sg_sgd_uob"),
+                ref("stg_bank_sg_sgd_uobcc")
+                ]
+        ) }}
+    )
+
+select *
+from union_all
