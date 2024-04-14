883c741606f08b2d281bcb7765afbade1ffb4952
jeremychia <jeremy.chia@vinted.com>
2024-04-14 20:34:22 +0200

    chore: lint fixes

--- models/marts/mart_transactions.sql
+++ models/marts/mart_transactions.sql
@@ -1,31 +1,32 @@
 {{ config(materialized="table") }}
 
-with unioned as (
-{{
-    dbt_utils.union_relations(
-        relations=[
-            ref("mart_eur_bank_transactions"),
-            ref("mart_sgd_bank_transactions"),
-        ],
-        source_column_name=None,
-    )
-}}
-),
+with
+    unioned as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    ref("mart_eur_bank_transactions"),
+                    ref("mart_sgd_bank_transactions"),
+                ],
+                source_column_name=None,
+            )
+        }}
+    ),
 
-renamed as (
-    select
-        source,
-        local_date,
-        local_currency,
-        local_amount,
-        coalesce(eur_currency, sgd_currency) as translated_currency,
-        coalesce(eur_amount, sgd_amount) as translated_amount,
-        category,
-        category2,
-        category3,
-        description
-    from unioned
-)
+    renamed as (
+        select
+            source,
+            local_date,
+            local_currency,
+            local_amount,
+            coalesce(eur_currency, sgd_currency) as translated_currency,
+            coalesce(eur_amount, sgd_amount) as translated_amount,
+            category,
+            category2,
+            category3,
+            description
+        from unioned
+    )
 
 select *
 from renamed
