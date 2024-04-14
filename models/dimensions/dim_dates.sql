949f3e4bd5b89be4d59d90de794b488d3d1927e5
jeremychia <jeremy.chia@vinted.com>
2024-04-14 10:00:03 +0200

    feat: introduce date table

---
+++ models/dimensions/dim_dates.sql
@@ -0,0 +1,9 @@
+with base_dates as ({{ dbt_date.get_base_dates('2018-01-01', '2025-01-01') }})
+
+select
+    cast(date_day as date) as local_date,
+    {{ dbt_date.day_of_week('date_day') }} as day_of_week_iso,
+    {{ dbt_date.day_of_month('date_day') }} as day_of_month,
+    {{ dbt_date.day_of_year("date_day") }} as day_of_year,
+    {{ dbt_date.iso_week_of_year("date_day") }} as iso_week_of_year
+from base_dates
