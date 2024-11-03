09a6fc7cf8ff0eaaf09af7243d60f3ef9834f883
jeremychia <jeremy.chia@vinted.com>
2024-11-03 13:22:29 +0100

    fix: missing exchange rates

--- models/facts/fx/fact_sgd_exchange_rates_long.sql
+++ models/facts/fx/fact_sgd_exchange_rates_long.sql
@@ -1,2 +1,23 @@
-select local_date, sgd, exchange_rate, currency
-from {{ ref("stg_fx_fx_sgd_from_eur_ecb") }}
+with source as (
+    select
+        local_date,
+        sgd,
+        exchange_rate,
+        currency
+    from {{ ref("stg_fx_fx_sgd_from_eur_ecb") }}
+),
+
+backfill_for_empty_dates as (
+    select
+        * except (exchange_rate),
+        coalesce(
+            exchange_rate,
+            lag(exchange_rate, 1)
+                over (partition by currency order by local_date),
+            lag(exchange_rate, 2)
+                over (partition by currency order by local_date)
+        ) as exchange_rate
+    from source
+)
+
+select * from backfill_for_empty_dates
